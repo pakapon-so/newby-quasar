3676739ac7d1895b5a78690405fe743afbd187be
KST-HATYAI\Pakapon.so <pakapon.so@kst-hatyai.com>
2024-07-02 11:18:04 +0700

    feat: เพิ่มภาพสไลด์เลื่อนในหน้า

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,39 @@
 <template>
+  <!-- สร้าง Carousel หรือ ภาพเลื่อน -->
   <q-page padding>
+    <div class="q-pa-md">
+      <q-carousel
+        animated
+        v-model="slide"
+        navigation
+        infinite
+        :autoplay="autoplay"
+        arrows
+        transition-prev="slide-right"
+        transition-next="slide-left"
+        @mouseenter="autoplay = false"
+        @mouseleave="autoplay = true"
+      >
+        <q-carousel-slide
+          :name="1"
+          img-src="https://cdn.quasar.dev/img/mountains.jpg"
+        />
+        <q-carousel-slide
+          :name="2"
+          img-src="https://cdn.quasar.dev/img/parallax1.jpg"
+        />
+        <q-carousel-slide
+          :name="3"
+          img-src="https://cdn.quasar.dev/img/parallax2.jpg"
+        />
+        <q-carousel-slide
+          :name="4"
+          img-src="https://cdn.quasar.dev/img/quasar.jpg"
+        />
+      </q-carousel>
+    </div>
+
+    <!-- แสดง Card -->
     <div class="row q-col-gutter-md">
       <card-component title="Our Changing Planet" subtitle="by John Doe">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque eveniet
@@ -23,6 +57,10 @@
 
 <script setup lang="ts">
 import CardComponent from 'src/components/CardComponent.vue';
+import { ref } from 'vue';
+
+const slide = ref(1);
+const autoplay = ref(true);
 
 defineOptions({
   name: 'IndexPage',
